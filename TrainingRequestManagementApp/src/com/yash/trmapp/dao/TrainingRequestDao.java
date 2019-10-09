db487fe6ad70e1aee95f3257cab57717f34b520a
dhananjay18 <46671132+dhananjay18@users.noreply.github.com>
2019-10-09 23:54:14 +0530

    Add files via upload

---
+++ TrainingRequestManagementApp/src/com/yash/trmapp/dao/TrainingRequestDao.java
@@ -0,0 +1,25 @@
+package com.yash.trmapp.dao;
+
+import com.yash.trmapp.domain.TrainingRequest;
+
+
+/**
+ * This is interface for traningRequestsDao
+ * @author Dhananjay
+ *
+ */
+public interface TrainingRequestDao {
+	
+	/**
+	 * This method is for saving the training requests
+	 * @param trainingRequest
+	 */
+	public void save(TrainingRequest trainingRequest);
+	
+	public void find(long id);
+	
+	public void update(long id);
+	
+	public void delete(long id);
+
+}
